f22707952c0e926e5f2bfcc659a63d6956d7e0ae
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2022-01-07 14:10:06 +0200

    Mesh Converter added (Helper folder)

---
+++ Simple_RPG/Assets/SCRIPTS/Helper/ConvertToRegularMesh.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertToRegularMesh : MonoBehaviour   // This is used only in the EDITOR
+{
+    [ContextMenu("Convert to regular mesh")] // <--- used in the EDITOR to convert the meshes
+    void Convert()
+    {
+        SkinnedMeshRenderer skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+
+        meshFilter.sharedMesh = skinnedMeshRenderer.sharedMesh;
+        meshRenderer.sharedMaterials = skinnedMeshRenderer.sharedMaterials;
+
+        DestroyImmediate(skinnedMeshRenderer);
+        DestroyImmediate(this);
+    }
+}
